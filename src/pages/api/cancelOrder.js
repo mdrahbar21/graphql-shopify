c5d8928c98d22de48f014918c4b0baa601c43572
Md Rahbar <mdrahbar21@iitk.ac.in>
2024-06-03 22:24:01 +0530

    stringify data

--- src/pages/api/cancelOrder.js
+++ src/pages/api/cancelOrder.js
@@ -1,22 +1,19 @@
 import { cancelOrder } from "@/lib/cancelOrder";
 
 export default async function handler(req, res) {
-    if (req.method !== 'POST') {
-        return res.status(405).json({ error: 'Method Not Allowed' });
-    }
 
     const { orderId } = req.body;
 
     if (!orderId) {
-        return res.status(400).json({ error: 'Order ID is required' });
+        return res.status(400).json({success:false, message: 'Order ID is required' });
     }
 
     const result = await cancelOrder(orderId);
 
     if (result.error) {
         console.error('Error cancelling order:', result.details);
-        return res.status(result.status).json({ error: result.error });
+        return res.status(result.status).json({success: false, message: result.error });
     }
-
-    return res.status(200).json(result.data);
+    const ans=JSON.stringify(result.data)
+    return res.status(200).json({success: false, message: ans});
 }
