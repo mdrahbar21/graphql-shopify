a256fcf0e8ba1d7f54f81dc81a727361123b3262
Md Rahbar <mdrahbar21@iitk.ac.in>
2024-06-03 17:44:03 +0530

    change orderbyphone

--- src/pages/api/orderbyphone.js
+++ src/pages/api/orderbyphone.js
@@ -1,13 +1,22 @@
 import { POST } from "@/lib/OrderByPhone";
 
 export default async function handler(req, res) {
-
-  const { phoneNumber } = req.body;
-  const result = await POST(phoneNumber);
-
-  if (result.error) {
-    return res.status(result.status).json({ error: result.error });
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method Not Allowed' });
   }
 
-  return res.status(200).json(result);
+  try {
+    const { phoneNumber } = await req.json(); 
+    const result = await POST(phoneNumber);
+
+    if (result.error) {
+      return res.status(result.status).json({ error: result.error });
+    }
+
+    // Respond with the fetched data
+    return res.status(200).json(result);
+  } catch (e) {
+    console.error("Server Error: ", e);
+    return res.status(500).json({ error: 'Server Error', message: e.toString() });
+  }
 }
