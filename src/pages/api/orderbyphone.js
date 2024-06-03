cb182b6d1cfdab603936a27499a3bb0ef681b935
Md Rahbar <mdrahbar21@iitk.ac.in>
2024-06-03 17:54:05 +0530

    change orderbyphone

--- src/pages/api/orderbyphone.js
+++ src/pages/api/orderbyphone.js
@@ -1,13 +1,9 @@
 import { POST } from "@/lib/OrderByPhone";
 
 export default async function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method Not Allowed' });
-  }
-
   try {
-    const { phoneNumber } = await req.json(); 
-    const result = await POST(phoneNumber);
+    const { phoneNum } = await req.json(); 
+    const result = await POST(phoneNum);
 
     if (result.error) {
       return res.status(result.status).json({ error: result.error });
