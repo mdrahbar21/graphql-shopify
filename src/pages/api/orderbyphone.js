4bd48b8c890a893338ccb4c8b55bcb5e54cf661f
Md Rahbar <mdrahbar21@iitk.ac.in>
2024-06-03 17:21:05 +0530

    changes in api/orderByPhone

--- src/pages/api/orderbyphone.js
+++ src/pages/api/orderbyphone.js
@@ -1,12 +1,9 @@
-import { getOrdersByPhone } from "@/internalServerFunctions/OrderByPhone";
+import { POST } from "@/internalServerFunctions/OrderByPhone";
 
 export default async function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res.status(405).end('Method Not Allowed');
-  }
 
   const { phoneNumber } = req.body;
-  const result = await getOrdersByPhone(phoneNumber);
+  const result = await POST(phoneNumber);
 
   if (result.error) {
     return res.status(result.status).json({ error: result.error });
